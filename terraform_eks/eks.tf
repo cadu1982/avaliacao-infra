2ac812b42770e5853a03d61270fdbe042519ba82
carlos <cadu.lp@gmail.com>
2023-08-04 11:29:34 -0300

    List cluster

--- terraform_eks/eks.tf
+++ terraform_eks/eks.tf
@@ -47,7 +47,7 @@ module "eks" {
   # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
     ami_type       = "AL2_x86_64"
-    instance_types = ["m5.large"]
+    instance_types = ["t3.large"]
 
     attach_cluster_primary_security_group = true
   }
