ff57acb9b786d1ffb74678e9183ade0260d45edd
carlos <cadu.lp@gmail.com>
2023-08-04 19:00:36 -0300

    [ci skip]

---
+++ terraform_eks/main.tf
@@ -0,0 +1,28 @@
+terraform {
+
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.2.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "entrega-infra-remote-state"
+    key    = "entrega-infra/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      owner      = "carlos"
+      managed-by = "Terraform"
+    }
+  }
+}
